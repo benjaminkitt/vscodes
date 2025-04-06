8e2a23b908eaaac6c39b3a746eddf2b4f5ca4dd8
jacek szymanski <jacek.p.szymanski@gmail.com>
2025-04-06 19:51:38 +0200

    fix: space in dataDir

--- vscodes.nix
+++ vscodes.nix
@@ -373,7 +373,7 @@ let
                     file_write="$file_write$([ "$file_write" != "" ] && echo "...")$profile"
                   done
 
-                  mkdir -p $(dirname "$file")
+                  mkdir -p "$(dirname "$file")"
                   echo "{}" > "$file"
                 fi
 
